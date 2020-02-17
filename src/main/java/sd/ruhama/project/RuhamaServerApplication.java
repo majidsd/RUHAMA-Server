3ff22c9dfe3e51705a23f8e224eb99d27ca60bf9
Abdalmajid Abdalla <abdalmaged.abdalla@gmail.com>
2020-02-17 12:22:54 +0200

    Initial commit, the basic spring project for Ruhama server side of application.

---
+++ src/main/java/sd/ruhama/project/RuhamaServerApplication.java
@@ -0,0 +1,13 @@
+package sd.ruhama.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuhamaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RuhamaServerApplication.class, args);
+	}
+
+}
