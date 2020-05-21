8f10763100c03fc633320f74b87da764d7989350
Abdalmajid Abdalla <abdalmaged.abdalla@gmail.com>
2020-05-21 21:57:09 +0200

    Edit WalletHistoryRepository
    Adding new two method,
    1. Method that return WalletHistory based on operation type.
    2. Method that return WalletHistory based on operation type and period
    of time.

--- src/main/java/net/ruhama/project/repo/WalletHistoryRepository.java
+++ src/main/java/net/ruhama/project/repo/WalletHistoryRepository.java
@@ -3,8 +3,13 @@
  */
 package net.ruhama.project.repo;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Date;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import net.ruhama.project.model.Wallet;
 import net.ruhama.project.model.WalletHistory;
 
 /**
@@ -12,5 +17,11 @@ import net.ruhama.project.model.WalletHistory;
  *
  */
 public interface WalletHistoryRepository extends JpaRepository<WalletHistory, Integer> {
+	
+	@Query("FROM WalletHistory where wallet = ?1 and operation = ?2")
+	public List<WalletHistory> getWalletOperationsHistory(Wallet wallet, Byte operation);
+	
+	@Query("FROM WalletHistory where wallet = ?1 and operation = ?2 and created_at between ?3 and ?4")
+	public List<WalletHistory> getWalletOperationsHistoryPerPeriod(Wallet wallet, Byte operation, Date fromDate, Date toDate);
 
 }
