b06496e8c376b7fb49615c9d9c6c1c5fbc697390
Abdalmajid Abdalla <abdalmaged.abdalla@gmail.com>
2020-05-22 03:33:46 +0200

    Initial commit for Donation Service Implementation.

---
+++ src/main/java/net/ruhama/project/dto/DonationDto.java
@@ -0,0 +1,136 @@
+/**
+ * 
+ */
+package net.ruhama.project.dto;
+
+import java.util.Date;
+
+/**
+ * @author MaJiD
+ *
+ */
+public class DonationDto {
+	private Integer id;
+	private Integer donator_id;
+	private Integer case_id;
+	private Double amount; 
+	private Date start_date;
+	private Date end_date;
+	private Integer new_case_id;
+	private String info;
+	private Integer created_by_id;
+	/**
+	 * @return the id
+	 */
+	public Integer getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	/**
+	 * @return the donator_id
+	 */
+	public Integer getDonator_id() {
+		return donator_id;
+	}
+	/**
+	 * @param donator_id the donator_id to set
+	 */
+	public void setDonator_id(Integer donator_id) {
+		this.donator_id = donator_id;
+	}
+	/**
+	 * @return the case_id
+	 */
+	public Integer getCase_id() {
+		return case_id;
+	}
+	/**
+	 * @param case_id the case_id to set
+	 */
+	public void setCase_id(Integer case_id) {
+		this.case_id = case_id;
+	}
+	/**
+	 * @return the amount
+	 */
+	public Double getAmount() {
+		return amount;
+	}
+	/**
+	 * @param amount the amount to set
+	 */
+	public void setAmount(Double amount) {
+		this.amount = amount;
+	}
+	/**
+	 * @return the start_date
+	 */
+	public Date getStart_date() {
+		return start_date;
+	}
+	/**
+	 * @param start_date the start_date to set
+	 */
+	public void setStart_date(Date start_date) {
+		this.start_date = start_date;
+	}
+	/**
+	 * @return the end_date
+	 */
+	public Date getEnd_date() {
+		return end_date;
+	}
+	/**
+	 * @param end_date the end_date to set
+	 */
+	public void setEnd_date(Date end_date) {
+		this.end_date = end_date;
+	}
+	/**
+	 * @return the new_case_id
+	 */
+	public Integer getNew_case_id() {
+		return new_case_id;
+	}
+	/**
+	 * @param new_case_id the new_case_id to set
+	 */
+	public void setNew_case_id(Integer new_case_id) {
+		this.new_case_id = new_case_id;
+	}
+	/**
+	 * @return the info
+	 */
+	public String getInfo() {
+		return info;
+	}
+	/**
+	 * @param info the info to set
+	 */
+	public void setInfo(String info) {
+		this.info = info;
+	}
+	/**
+	 * @return the created_by_id
+	 */
+	public Integer getCreated_by_id() {
+		return created_by_id;
+	}
+	/**
+	 * @param created_by_id the created_by_id to set
+	 */
+	public void setCreated_by_id(Integer created_by_id) {
+		this.created_by_id = created_by_id;
+	}
+	@Override
+	public String toString() {
+		return "DonationDto [donator_id=" + donator_id + ", case_id=" + case_id + ", amount=" + amount + ", start_date="
+				+ start_date + ", end_date=" + end_date + ", new_case_id=" + new_case_id + ", info=" + info
+				+ ", created_by_id=" + created_by_id + "]";
+	}
+}
---
+++ src/main/java/net/ruhama/project/serviceImp/DonationServiceImp.java
@@ -0,0 +1,226 @@
+/**
+ * 
+ */
+package net.ruhama.project.serviceImp;
+
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import net.ruhama.project.dto.DonationDto;
+import net.ruhama.project.dto.WalletDto;
+import net.ruhama.project.model.Case;
+import net.ruhama.project.model.Donation;
+import net.ruhama.project.model.User;
+import net.ruhama.project.model.Wallet;
+import net.ruhama.project.repo.CaseRepository;
+import net.ruhama.project.repo.DonationRepository;
+import net.ruhama.project.repo.UserRepository;
+import net.ruhama.project.response.ListResponse;
+import net.ruhama.project.response.ObjectResponse;
+import net.ruhama.project.service.IDonationService;
+import net.ruhama.project.service.IWalletService;
+import net.ruhama.project.util.ResponseEnum;
+
+/**
+ * @author MaJiD
+ *
+ */
+@Service
+public class DonationServiceImp implements IDonationService {
+	
+	@Autowired
+	private DonationRepository donationRepository;
+	
+	@Autowired
+	private IWalletService walletService;
+	
+	@Autowired
+	private CaseRepository caseRepository;
+	
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public ObjectResponse<Donation> donate(DonationDto donationDto) {
+		ObjectResponse<Donation> response;
+		Case the_case;
+		Wallet wallet;
+		User doner;
+		Donation donation;
+		User creater;
+		try {
+			if(donationDto.getAmount() > 0) {
+				doner = userRepository.getOne(donationDto.getDonator_id());
+				wallet = walletService.getWallet(doner.getId()).getDto();
+				the_case = caseRepository.getOne(donationDto.getCase_id());
+				creater = userRepository.getOne(donationDto.getCreated_by_id());
+				
+				if(wallet != null ) {
+					if(wallet.getCurrent_balance() >= donationDto.getAmount()) {
+						donation = new Donation();
+						donation.setAmount(donationDto.getAmount());
+						donation.setCreated_at(new Date());
+						donation.setLast_update(new Date());
+						donation.setDonator(doner);
+						donation.setThe_case(the_case);
+						donation.setInfo(donationDto.getInfo());
+						donation.setCreated_by(creater);
+						donation.setLast_update_by(creater);
+						
+						WalletDto walletDto = new WalletDto();
+						walletDto.setAmount(donationDto.getAmount());
+						walletDto.setOwner_id(donationDto.getDonator_id());
+						walletDto.setCreated_by_id(donationDto.getCreated_by_id());
+						walletDto.setId(wallet.getId());
+						
+						the_case.setCurrentDonations(the_case.getCurrentDonations() + donationDto.getAmount());
+						the_case.setDonorsCount(the_case.getDonorsCount() + 1);
+						the_case.setUpdatedAt(new Date());
+						
+						ObjectResponse<Wallet> walletResponse = walletService.debit(walletDto);
+						if(walletResponse.getResponseCode() == ResponseEnum.SUCCESS.getResponseCode()) {
+							Donation savedDonation = donationRepository.save(donation);
+							caseRepository.save(the_case);
+							response = new ObjectResponse<Donation>(ResponseEnum.SUCCESS, savedDonation);
+						} else {
+							response = new ObjectResponse<Donation>(ResponseEnum.TRY_AGAIN, null);
+						}
+						
+					} else {
+						response = new ObjectResponse<Donation>(ResponseEnum.INSUFFICIENT, null);
+					}
+				} else {
+					response = new ObjectResponse<Donation>(ResponseEnum.ITEM_NOT_FOUND, null);
+				}
+			} else {
+				response = new ObjectResponse<Donation>(ResponseEnum.NEGATIVE_AMOUNT, null);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			response = new ObjectResponse<Donation>(ResponseEnum.TRY_AGAIN, null);
+		}
+		return response;
+	}
+
+	@Override
+	public ObjectResponse<Donation> transferDonation(DonationDto donationDto) {
+		ObjectResponse<Donation> response;
+		Case old_case;
+		Case new_case;
+		Donation donation;
+		try {
+			donation = donationRepository.getOne(donationDto.getId());
+			if(donation != null) {
+				old_case = caseRepository.getOne(donationDto.getCase_id());
+				new_case = caseRepository.getOne(donationDto.getNew_case_id());
+				if(old_case != null && new_case != null) {
+					if(donation.getThe_case().getId() == old_case.getId()) {
+						new_case.setCurrentDonations(new_case.getCurrentDonations() + donation.getAmount());
+						new_case.setDonorsCount(new_case.getDonorsCount() + 1);
+						new_case.setUpdatedAt(new Date());
+						
+						old_case.setCurrentDonations(old_case.getCurrentDonations() - donation.getAmount());
+						old_case.setDonorsCount(old_case.getDonorsCount() - 1);
+						old_case.setUpdatedAt(new Date());
+						
+						donation.setThe_case(new_case);
+						donation.setLast_update(new Date());
+						donation.setLast_update_by(userRepository.getOne(donationDto.getCreated_by_id()));
+						
+						Donation updatedDonation = donationRepository.save(donation);
+						if(updatedDonation != null) {
+							caseRepository.save(new_case);
+							caseRepository.save(old_case);
+							response = new ObjectResponse<Donation>(ResponseEnum.SUCCESS, updatedDonation);
+						} else {
+							response = new ObjectResponse<Donation>(ResponseEnum.TRY_AGAIN, null);
+						}
+					} else {
+						response = new ObjectResponse<Donation>(ResponseEnum.ITEM_NOT_FOUND, null);
+					}
+				} else {
+					response = new ObjectResponse<Donation>(ResponseEnum.ITEM_NOT_FOUND, null);
+				}
+			} else {
+				response = new ObjectResponse<Donation>(ResponseEnum.ITEM_NOT_FOUND, null);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			response = new ObjectResponse<Donation>(ResponseEnum.TRY_AGAIN, null);
+		}
+		return response;
+	}
+
+	@Override
+	public ListResponse<Donation> getCaseDonations(DonationDto donationDto) {
+		ListResponse<Donation> response;
+		try {
+			Case the_case = caseRepository.getOne(donationDto.getCase_id());
+			if(the_case != null) {
+				response = new ListResponse<Donation>(ResponseEnum.SUCCESS, donationRepository.getCaseDonations(the_case));
+			} else {
+				response = new ListResponse<Donation>(ResponseEnum.ITEM_NOT_FOUND, null);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			response = new ListResponse<Donation>(ResponseEnum.TRY_AGAIN, null);
+		}
+		return response;
+	}
+
+	@Override
+	public ListResponse<Donation> getCaseDonationsPerPeriod(DonationDto donationDto) {
+		ListResponse<Donation> response;
+		try {
+			Case the_case = caseRepository.getOne(donationDto.getCase_id());
+			if(the_case != null) {
+				response = new ListResponse<Donation>(ResponseEnum.SUCCESS, donationRepository.getCaseDonationsPerPeriod(the_case, donationDto.getStart_date(), donationDto.getEnd_date()));
+			} else {
+				response = new ListResponse<Donation>(ResponseEnum.ITEM_NOT_FOUND, null);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			response = new ListResponse<Donation>(ResponseEnum.TRY_AGAIN, null);
+		}
+		return response;
+	}
+
+	@Override
+	public ListResponse<Donation> getUserDonations(DonationDto donationDto) {
+		ListResponse<Donation> response;
+		try {
+			User donor = userRepository.getOne(donationDto.getDonator_id());
+			if(donor != null) {
+				response = new ListResponse<Donation>(ResponseEnum.SUCCESS, donationRepository.getUserDonations(donor));
+			} else {
+				response = new ListResponse<Donation>(ResponseEnum.ITEM_NOT_FOUND, null);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			response = new ListResponse<Donation>(ResponseEnum.TRY_AGAIN, null);
+		}
+		return response;
+	}
+
+	@Override
+	public ListResponse<Donation> getUserDonationsPerPeriod(DonationDto donationDto) {
+		ListResponse<Donation> response;
+		try {
+			User donor = userRepository.getOne(donationDto.getDonator_id());
+			if(donor != null) {
+				response = new ListResponse<Donation>(ResponseEnum.SUCCESS, donationRepository.getUserDonationsPerPeriod(donor, donationDto.getStart_date(), donationDto.getEnd_date()));
+			} else {
+				response = new ListResponse<Donation>(ResponseEnum.ITEM_NOT_FOUND, null);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			response = new ListResponse<Donation>(ResponseEnum.TRY_AGAIN, null);
+		}
+		return response;
+	}
+
+	
+
+}
