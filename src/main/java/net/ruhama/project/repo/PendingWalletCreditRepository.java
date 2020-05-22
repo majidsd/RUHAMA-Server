e3240e35c2a07722745219e6c17bbe575012434a
Abdalmajid Abdalla <abdalmaged.abdalla@gmail.com>
2020-05-22 20:16:49 +0200

    Edit Pending Wallet Credit Repository
    - Adding new two methods for querying the database, as follow:
    * getWalletPendingCredit return wallet pending items.
    * getWalletsPendingCredit return all pending items.

--- src/main/java/net/ruhama/project/repo/PendingWalletCreditRepository.java
+++ src/main/java/net/ruhama/project/repo/PendingWalletCreditRepository.java
@@ -3,14 +3,24 @@
  */
 package net.ruhama.project.repo;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import net.ruhama.project.model.PendingWalletCredit;
+import net.ruhama.project.model.Wallet;
 
 /**
  * @author MaJiD
  *
  */
 public interface PendingWalletCreditRepository extends JpaRepository<PendingWalletCredit, Integer> {
-
+	
+	@Query("FROM PendingWalletCredit where wallet = ?1 and status = ?2")
+	public List<PendingWalletCredit> getWalletPendingCredit(Wallet wallet, Byte status);
+	
+	@Query("FROM PendingWalletCredit where status = ?1")
+	public List<PendingWalletCredit> getWalletsPendingCredit(Byte status);
+	
 }
