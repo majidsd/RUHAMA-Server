0769cee0ac942dbb3e2412ce4e94274cacb9b7b1
Abdalmajid Abdalla <abdalmaged.abdalla@gmail.com>
2020-05-30 23:56:37 +0200

    Initial commit for Donation and Wallet controllers.

---
+++ src/main/java/net/ruhama/project/controller/api/WalletApiController.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package net.ruhama.project.controller.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author MaJiD
+ *
+ */
+@RestController
+@RequestMapping("/api/wallet")
+public class WalletApiController {
+
+}
