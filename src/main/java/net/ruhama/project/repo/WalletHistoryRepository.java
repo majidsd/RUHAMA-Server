84408a8b0206117e4b07efe852a51adccc03c2ec
Abdalmajid Abdalla <abdalmaged.abdalla@gmail.com>
2020-05-30 23:09:17 +0200

    Adding new two method to WalletHistory Repository.
    - getWalletTransactions by wallet.
    - getWalletTransactionsPerPeriod by wallet and period of time.

--- src/main/java/net/ruhama/project/repo/WalletHistoryRepository.java
+++ src/main/java/net/ruhama/project/repo/WalletHistoryRepository.java
@@ -24,4 +24,10 @@ public interface WalletHistoryRepository extends JpaRepository<WalletHistory, In
 	@Query("FROM WalletHistory where wallet = ?1 and operation = ?2 and created_at between ?3 and ?4")
 	public List<WalletHistory> getWalletOperationsHistoryPerPeriod(Wallet wallet, Byte operation, Date fromDate, Date toDate);
 
+	@Query("FROM WalletHistory where wallet = ?1")
+	public List<WalletHistory> getWalletTransactions(Wallet wallet);
+	
+	@Query("FROM WalletHistory where wallet = ?1 and operation = ?2 and created_at between ?3 and ?4")
+	public List<WalletHistory> getWalletTransactionsPerPeriod(Wallet wallet, Date fromDate, Date toDate);
+
 }
