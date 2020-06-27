8f5dfa64ac81d506db9db371044230e524386b1d
ahmedozy <ahmedozy@gmail.com>
2020-06-28 00:34:11 +0200

    Unit Test Hold
    commented all the tests

--- src/test/java/net/ruhama/project/services/UserServiceUnitTest.java
+++ src/test/java/net/ruhama/project/services/UserServiceUnitTest.java
@@ -3,16 +3,7 @@
  */
 package net.ruhama.project.services;
 
-import java.util.Arrays;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import net.ruhama.project.dto.AuthorityDto;
-import net.ruhama.project.dto.UserDto;
-import net.ruhama.project.response.ObjectResponse;
-import net.ruhama.project.service.IUserService;
 
 /**
  * @author ahmedozy
@@ -20,14 +11,15 @@ import net.ruhama.project.service.IUserService;
  */
 @SpringBootTest
 public class UserServiceUnitTest {
-	@Autowired
-	private IUserService userService;
-	
-	@Test
-	public void addUserTest() {
-		UserDto userDto = new UserDto("Ahmed Osman Wahbi", "ahmedozy", "password", "password",  Arrays.asList(new AuthorityDto[] {new AuthorityDto(null,"admin","admin user")}));
-		userDto.setPhoneNumber(116300001);
-		ObjectResponse<UserDto> response = userService.addUser(userDto);
-		System.out.println(response);
-	}
+	/*
+	 * @Autowired private IUserService userService;
+	 * 
+	 * 
+	 * @Test public void addUserTest() { UserDto userDto = new
+	 * UserDto("Ahmed Osman Wahbi", "ahmedozy", "password", "password",
+	 * Arrays.asList(new AuthorityDto[] {new
+	 * AuthorityDto(null,"admin","admin user")}));
+	 * userDto.setPhoneNumber(116300001); ObjectResponse<UserDto> response =
+	 * userService.addUser(userDto); System.out.println(response); }
+	 */
 }
